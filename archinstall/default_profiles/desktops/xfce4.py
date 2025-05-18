f4c1e2ecfe558f81e6cb211b96cf7b993df0e0a8
bojanstrkovski-21 <bojanstrkovski.21@gmail.com>
2025-05-18 19:26:04 +0200

    add archinstall boki

---
+++ archinstall/default_profiles/desktops/xfce4.py
@@ -0,0 +1,31 @@
+from typing import override
+
+from archinstall.default_profiles.profile import GreeterType, ProfileType
+from archinstall.default_profiles.xorg import XorgProfile
+
+
+class Xfce4Profile(XorgProfile):
+	def __init__(self) -> None:
+		super().__init__("Xfce4", ProfileType.DesktopEnv)
+
+	@property
+	@override
+	def packages(self) -> list[str]:
+		return [
+			"base-devel",
+			"btop"
+			"duf"
+			"firefox"
+			"ghostty"
+			"neovim"
+			"nfs-utils"
+			"xfce4-arch-boki-meta",
+			"archboki-xfce",
+			"archboki-shells",
+			"archboki-ghostty-config",
+		]
+
+	@property
+	@override
+	def default_greeter_type(self) -> GreeterType:
+		return GreeterType.Sddm
